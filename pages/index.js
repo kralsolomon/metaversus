830ce81cec7be1b99912c722ea97232460e72610
kralsolomon <solomonkein228@gmail.com>
2023-08-22 09:04:45 +0600

    ESLint correction

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,32 @@ import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World }
 
 const Page = () => (
 
-    <div className="bg-primary-black overflow-hidden">
-        <Navbar />
-        <Hero />
+  <div className="bg-primary-black overflow-hidden">
+    <Navbar />
+    <Hero />
 
-        <div className="relative">
-            <About />
-            <div className="gradient-03 z-0 " />
-            <Explore />
-        </div>
-
-        <div className="relative">
-            <GetStarted />
-            <div className="gradient-04 z-0 " />
-            <WhatsNew />
-        </div>
-
-        <World />
-
-        <div className="relative">
-            <Insights />
-            <div className="gradient-04 z-0 " />
-            <Feedback />
-        </div>
-
-        <Footer />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0 " />
+      <Explore />
     </div>
+
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-04 z-0 " />
+      <WhatsNew />
+    </div>
+
+    <World />
+
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0 " />
+      <Feedback />
+    </div>
+
+    <Footer />
+  </div>
 );
 
 export default Page;
